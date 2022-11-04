691eb46ba334ad98ffec996d0ad9341712b5a929
GJHeyes <gregory-heyes@hotmail.com>
2022-11-04 11:48:34 +0000

    Added tests

--- tests/cheese.test.js
+++ tests/cheese.test.js
@@ -1,10 +1,24 @@
-const Cheese = require('./cheese.models')
-const Board = require('./board.models')
-const User = require('./user.models')
+//const Cheese = require('./cheese.models')
+const Cheese = require('../models/cheese.models')
+const db = require('../db/db')
+//const User = require('./user.models')
 
-describe('Testing cheese', ()=>{
-
-    test('',()=>{
-        
+describe('Testing the user',()=>{
+    beforeEach(async()=>{//before each test
+        await db.sync({
+            force: true
+        })
     })
+    
+    test('Check a board can be created',async()=>{
+       const cheese = await Cheese.create({title:'Mozzarella', description: `Mozzarella is a fresh, soft, stretched curd cheese, made with whole cow's milk. Due to the fact that it has a fragrant aroma of fresh milk and a delicate creamy flavor, mozzarella is traditionally paired with light white wines. This Italian classic originates from the region of Campania, but nowadays it is produced all across the country.
+
+       The cheese is made with water buffalo's milk or cow's milk, when it's often referred to as mozzarella fior di latte (in order to distinguish it from water buffalo milk mozzarella). The ancient tradition of making mozzarella cheese dates back to the 4th century BCE, however, the first official reference to its name was found in a 1570 cookery book by Bartolomeo Scappi, a famous Renaissance chef.
+       
+       It is an excellent table cheese used for preparing a number of different dishes and the essential ingredient of the caprese salad and the famous pizza napoletana.`})
+       expect(cheese.title).toBe('Mozzarella')
+       expect(cheese.description).toBeTruthy()
+       expect(cheese.id).toBeTruthy() 
+    })
+
 })
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,10 +1,19 @@
-const Cheese = require('./cheese.models')
-const Board = require('./board.models')
-const User = require('./user.models')
+//const Cheese = require('../models/cheese.models')
+//const Board = require('./models/board.models')
+const User = require('../models/user.models')
+const db = require('../db/db')
 
-describe('Testing user', ()=>{
-
-    test('',()=>{
-        
+describe('Testing the user', ()=>{
+    beforeEach(async()=>{//before each test
+        await db.sync({
+            force: true
+        })
     })
+    
+    test('Check a user can be created',async()=>{
+       const user = await User.create({name:'Greg', email: 'test@test.co.uk'})
+       expect(user.name).toBe('Greg')
+       expect(user.id).toBeTruthy() 
+    })
+
 })
